1da8ccc359258b56bce62e82986250ae37e9a2e4
geigea84 <geigea84@gmail.com>
2021-02-15 20:54:26 -0600

    laid out format for index.js prompts

--- index.js
+++ index.js
@@ -9,3 +9,116 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+//import template
+const template = require('./src/template');
+
+//https://nodejs.org/api/path.html
+//generate a new directory on html build
+const teamFolder = path.resolve(__dirname, "team") ;
+
+//build html file in dynamically created directory
+//(connect to end of /team/ path)
+const teamPath = path.join(teamFolder, "index.html");
+
+//team members array?
+//blank array to hold pushed team info
+const teamArr = [];
+
+//id array?
+
+//initialize inquirer prompt in CLI
+//have unique names for each question
+function init() {
+    //inquirer prompts to add manager
+    //manager’s name, employee ID, email address, and office number
+    function addManager() {
+        console.log('Build your team');
+        inquirer.prompt([
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            }
+        ])
+    }
+    
+    //manager options: add engineer, add intern, done adding
+    function addTeamMember() {
+        inquirer.prompt([
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+        ])
+    }
+
+    //prompts to add engineer
+    //engineer’s name, ID, email, and GitHub username
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            }
+        ])
+    }
+
+    //prompts to add intern
+    //intern’s name, ID, email, and school
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            },
+            {
+                type: '',
+                name: '',
+                message: ''
+            }
+        ])
+    }
+}
